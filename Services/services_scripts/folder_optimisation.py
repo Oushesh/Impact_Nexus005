adce66115a6ec3a6bb22356332ff70b2011bc112
oushesh <oushesh@gmail.com>
2023-11-16 17:53:23 +0400

    updated

--- Services/services_scripts/folder_optimisation.py
+++ Services/services_scripts/folder_optimisation.py
@@ -3,10 +3,6 @@ import pandas as pd
 import json
 from pathlib import Path
 
-import os
-import pandas as pd
-import json
-
 def find_files_in_folder(folder_path):
     # Recursively find all files in the folder and its subfolders
     files = []
@@ -15,59 +11,75 @@ def find_files_in_folder(folder_path):
             files.append(os.path.join(root, filename))
     return files
 
-def convert_to_parquet(folder_path, input_paths, main_output_folder):
-    for input_path in input_paths:
+def convert_to_parquet(input_base_folder, input_file_paths, output_base_folder):
+    for input_file_path in input_file_paths:
         # Check if the file is empty
-        if os.path.getsize(input_path) == 0:
-            print(f"Skipping empty file: {input_path}")
+        if os.path.getsize(input_file_path) == 0:
+            #Add logger here
+            print(f"Skipping empty file: {input_file_path}")
             continue
 
         # Determine file format based on file extension
-        file_extension = input_path.split('.')[-1].lower()
-        file_name = input_path.split(".")[0].split("/")[-1].lower()
+        file_extension = input_file_path.split('.')[-1].lower()
+        file_name = input_file_path.split(".")[0].split("/")[-1]
+
+        output_file_name = input_file_path.split(".")[0].split("/")[-1].lower() + ".parquet"
+        output_relative_folder = input_file_path.split(input_base_folder)[-1].split(file_name+"."+file_extension)[0]
+        output_relative_folder = output_relative_folder.strip("/")
+
+        output_folder = os.path.join(output_base_folder,output_relative_folder)
+        print (file_name+"."+file_extension)
+        print ("file extension",file_extension)
+        print ("file name",file_name)
+        print ("output file_name",output_file_name)
+        print ("output_relative_folder",output_relative_folder)
+        print ("input_file_path",input_file_path)
+        print ("output_base_folder",output_base_folder)
+        print ("output_folder",output_folder)
         try:
             if file_extension in ['csv', 'tsv', 'txt']:
                 # CSV, TSV, TXT to Parquet
                 sep = '\t' if file_extension == 'tsv' else ',' if file_extension == 'csv' else None
-                df = pd.read_csv(input_path, sep=sep, header=None)
+                df = pd.read_csv(input_file_path, sep=sep, header=None)
                 if df.shape[1] == 1:
                     df.columns = ['HEADER']
 
-                output_folder_path = os.path.join(main_output_folder,input_path.split(folder_path)[-1])
-                os.makedirs(os.path.dirname(output_folder_path), exist_ok=True)
-
-                output_path = os.path.join(output_folder_path,file_name+".parquet")
+                try:
+                    os.makedirs(output_folder, exist_ok=True)
+                except OSError as e:
+                    print(f'Error creating the directory: {e}')
 
             elif file_extension == 'json':
                 # JSON to Parquet
-                df = pd.read_json(input_path)
+                df = pd.read_json(input_file_path)
 
-                output_folder_path = os.path.join(main_output_folder,input_path.split(folder_path)[-1])
-                os.makedirs(os.path.dirname(output_folder_path), exist_ok=True)
-                output_path = os.path.join(output_folder_path,file_name+".parquet")
+                try:
+                    os.makedirs(output_folder, exist_ok=True)
+                except OSError as e:
+                    print(f'Error creating the directory: {e}')
 
             elif file_extension == 'jsonl':
                 # JSONL to Parquet
                 data = []
-                with open(input_path, 'r') as file:
+                with open(input_file_path, 'r') as file:
                     for line in file:
                         data.append(json.loads(line))
                 df = pd.DataFrame(data)
 
-                output_folder_path = os.path.join(main_output_folder,input_path.split(folder_path)[-1])
-                os.makedirs(os.path.dirname(output_folder_path), exist_ok=True)
-
-                output_path = os.path.join(output_folder_path,file_name+".parquet")
+                try:
+                    os.makedirs(output_folder, exist_ok=True)
+                except OSError as e:
+                    print(f'Error creating the directory: {e}')
 
             elif file_extension == 'xlsx':
                 # Excel (xlsx) to Parquet
-                df = pd.read_excel(input_path, header=None)
+                df = pd.read_excel(input_file_path, header=None)
                 if df.shape[1] == 1:
                     df.columns = ['HEADER']
-                output_folder_path = os.path.join(main_output_folder,input_path.split(folder_path)[-1])
-                os.makedirs(os.path.dirname(output_folder_path), exist_ok=True)
-
-                output_path = os.path.join(output_folder_path,file_name+".parquet")
+                try:
+                    os.makedirs(output_folder, exist_ok=True)
+                except OSError as e:
+                    print(f'Error creating the directory: {e}')
 
             else:
                 print(f"Unsupported file format: {file_extension}")
@@ -83,20 +95,23 @@ def convert_to_parquet(folder_path, input_paths, main_output_folder):
             df.columns = df.columns.astype(str)
 
             # Write DataFrame to Parquet
-            df.to_parquet(output_path)
+            df.to_parquet(os.path.join(output_folder,output_file_name))
 
         except pd.errors.ParserError as e:
-            print(f"Error parsing {file_extension} file {input_path}: {e}")
+            print(f"Error parsing {file_extension} file {input_file_path}: {e}")
             # Handle the error as needed (skip the file, log the error, etc.)
 
 if __name__ == "__main__":
     # Example usage:
     BASE_DIR = Path(__file__).resolve().parent.parent
-    input_folder = os.path.join(BASE_DIR,"services_app/KnowledgeBase")  # Replace with your input folder
-    print (input_folder)
-    output_folder = os.path.join(BASE_DIR,"services_app/KnowledgeBaseParquet")  # Replace with your desired output folder
+    input_base_folder = os.path.join(BASE_DIR,"services_app/KnowledgeBase")  # Replace with your input folder
 
+    print ("input_folder",input_base_folder)
+    output_base_folder = os.path.join(BASE_DIR,"services_app/KnowledgeBaseParquet")  # Replace with your desired output folder
+
+    print ("output_folder",output_base_folder)
     # Find all files in the input folder and its subfolders
-    input_files = find_files_in_folder(input_folder)
+    input_file_paths = find_files_in_folder(input_base_folder)
+    print ("input_file",input_file_paths[10])
     # Convert each file to Parquet and save it in the output folder
-    convert_to_parquet(input_folder,input_files, output_folder)
+    convert_to_parquet(input_base_folder,input_file_paths, output_base_folder)
