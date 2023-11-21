ec8378c6af1cbc9f8585a2acc43965f214dbf86b
oushesh <oushesh@gmail.com>
2023-11-21 18:47:29 +0400

    new services, requirements, no tika.log

---
+++ Services/services_app/api/routers/data_processor_classification_job.py
@@ -0,0 +1,53 @@
+"""
+This service allows to convert any
+raw data in the form of .csv,
+.tsv or any excel sheet into ready for the job
+data.
+"""
+
+import os
+from ninja import Router
+from ninja.files import UploadedFile
+import pandas as pd
+from pathlib import Path
+import json
+import supabase
+import logging
+import io
+from dotenv import load_dotenv
+from datetime import datetime
+
+
+router = Router()
+
+# Configure logging
+log_file_path = "data_processor_classification_logs.log"
+logging.basicConfig(level=logging.INFO, filename=log_file_path, filemode='a',
+                    format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+logger = logging.getLogger(__name__)
+
+@router.post("/process_file_classification_job")
+def process_file_classification_job(request, file: UploadedFile):
+    # Read the file into a Pandas DataFrame
+    if file.name.endswith('.xlsx'):
+        df = pd.read_excel(file.file, engine='openpyxl')
+    elif file.name.endswith(('.csv', '.tsv')):
+        delimiter = '\t' if file.name.endswith('.tsv') else ','
+        df = pd.read_csv(io.StringIO(file.file.read().decode('utf-8')), delimiter=delimiter)
+    else:
+        return HttpResponse("Unsupported file format. Please upload a CSV, TSV, or Excel file.", status=400)
+
+    # Fill in empty rows with "None"
+    df.fillna('None', inplace=True)
+
+    # Save the modified DataFrame to a new Excel file
+    output_file = io.BytesIO()
+    df.to_excel(output_file, index=False, engine='openpyxl')
+    output_file.seek(0)
+
+    # Create a response with the modified file
+    response = HttpResponse(output_file.read(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=modified_file.xlsx'
+
+    return response
--- Services/services_project/urls.py
+++ Services/services_project/urls.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI, Schema
+
 from services_app.api.routers.build_knowledgebase import router as build_knowledgebase_router
 from services_app.api.routers.parquet_conversion import router as parquet_conversion_router
 from services_app.api.routers.model_downloader import router as model_downloader_router
+from services_app.api.routers.data_processor_classification_job import router as data_processor_classification_job_router
+
 
 #Call the routers from ninja
 api = NinjaAPI()
 api.add_router("/",build_knowledgebase_router)
 api.add_router("/",parquet_conversion_router)
 api.add_router("/downloader",model_downloader_router)
+api.add_router("/jobs",data_processor_classification_job_router)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
