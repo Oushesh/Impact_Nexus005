ad8997de02b664b118c74e19293a4623a1732818
oushesh <oushesh@gmail.com>
2023-11-25 22:08:08 +0400

    debug

--- Services/services_app/api/routers/deepcheck_data_properties.py
+++ Services/services_app/api/routers/deepcheck_data_properties.py
@@ -4,10 +4,9 @@ the embeddings and stats from incoming data
 It uses but can also be modified to use openai ada embeddings
 """
 
-import os
-
-import pandas
 from ninja import Router
+from ninja import File
+
 import pandas as pd
 import json
 import supabase
@@ -20,9 +19,7 @@ from pathlib import Path
 from services_app.api.utils.DeepChecks.get_data_properties_deepcheck import DisplayEmbeddings
 from deepchecks.nlp import TextData
 from deepchecks.nlp.checks import TextPropertyOutliers
-
-
-router = Router()
+from django.http import JsonResponse
 
 # Configure logging
 log_file_path = "deepcheck_incoming_data.log"
@@ -35,41 +32,53 @@ logger = logging.getLogger(__name__)
 ## any cloud database such as Snowflake, s3 or cloud computing platform
 
 
-clsss Compute_Data_Properties:
+
+
+router = Router()
+
+class Data_Properties:
     def __init__(self,**kwargs):
         self.kwargs = kwargs
 
-
     @classmethod
-    def compute_properties(cls,filepath:str):
-        #TODO: test if filepath is indeed a path
-        data = pandas.read_csv(filepath)
+    def compute(cls, file):
+        # Access the file content using file.read()
+        file_content = file.read()
+
+        # Use pandas to read CSV data from the file content
+        data = pd.read_csv(io.StringIO(file_content.decode('utf-8')))
 
         try:
             text_data = TextData(data["text"])
         except Exception as error:
             # Handle the exception here
             print(f"Error: {error}")
+            return JsonResponse({"message": f"Error processing file: {str(error)}"}, status=500)
 
-        #TODO: we have a schema for the definition of the incoming data
+        # TODO: Define a schema for the incoming data
         text_data.calculate_builtin_properties()
         TextPropertyOutliers().run(text_data)
-        text_data.save_properties(f"{filepath}_properties.csv")
+
+        # Save properties using the original filename
+        properties_filename = f"{file.name}_properties.csv"
+        text_data.save_properties(properties_filename)
 
         check = TextPropertyOutliers()
         result = check.run(text_data)
-        result.save_as_html(file=os.path.join(filepath.split(".csv")[0],"_properties.html"))
+
+        # Save result as HTML using the original filename
+        html_filename = f"{file.name.split('.csv')[0]}_properties.html"
+        result.save_as_html(file=html_filename)
         result.to_wandb()
 
+        #TODO: modify return here
         return None
 
-@router.get("/deepcheck"))
-def deepcheck(request,filename:str,url:directory="Data"):
-    # TODO: fetch processed data from: the other endpoint
+@router.post("/deepcheck_endpoint")
+def deepcheck(request, file: File):
+    Data_Properties.compute(file)
+    return JsonResponse({"message":"File processed successfully"},status=200)
 
-
-    Compute_Data_Properties.compute_properties()
-
-    return None
-
-
+## TODO: Complete this endpoint.
+## https://github.com/edemiraydin/snowpark_ml_demo_deepchecks/blob/main/Linear%20Regression%20with%20Snowpark%20and%20DeepChecks.ipynb
+## Apache Airflow for automation
