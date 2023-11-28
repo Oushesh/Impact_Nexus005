06693a65fca563258c5cdd3988a86dc6e15bad1e
oushesh <oushesh@gmail.com>
2023-11-28 15:20:56 +0400

    updated Services

--- Services/DVC/linear_regression_test/test.py
+++ Services/DVC/linear_regression_test/test.py
@@ -2,7 +2,7 @@ import pickle
 from sklearn.datasets import make_regression
 import json
 
-model = pickle.load(open("models/model.pkl", "rb"))
+model = pickle.load(open("linear_regression/models/model.pkl", "rb"))
 
 # Generate some data for validation
 X_test, y = make_regression(1000,n_features = 10)
