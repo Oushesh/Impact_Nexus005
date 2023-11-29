3d4d14a7417a7161982c607a540c309751fe442c
oushesh <oushesh@gmail.com>
2023-11-29 13:00:13 +0400

    train.py with lasso

--- Services/DVC/test/train.py
+++ Services/DVC/test/train.py
@@ -7,8 +7,8 @@ import os
 X, y = make_regression(10000,n_features = 10)
 
 # Train a model
-reg = LinearRegression().fit(X, y.ravel())
-#reg = Lasso().fit(X, y.ravel())
+#reg = LinearRegression().fit(X, y.ravel())
+reg = Lasso().fit(X, y.ravel())
 # Print out training r2
 print(reg.score(X,y.ravel() ))
 
@@ -18,5 +18,3 @@ if not os.path.isdir("models/"):
 
 filename = 'models/model.pkl'
 pickle.dump(reg, open(filename, 'wb'))
-
-# TODO: the model is then changed
